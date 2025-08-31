8f2e50f4f31baf6bd724a8eb6cf8fc7f7088324a
Emirhan Tandoğan <emirhantandogann@gmail.com>
2025-08-31 19:32:21 +0300

    fix images

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -16,7 +16,7 @@ export default function Intro() {
                     </div>
                 </div>
                 <div className="col-sm-12 col-md-5 text-center py-5 text-white">
-                  <Image src="/images/profile.jpg" className="img-fluid rounded-4 shadow" width={600} height={600} style={{ width: "30vh" }} alt="Profile"/>
+                  <Image src="./images/profile.jpg" className="img-fluid rounded-4 shadow" width={600} height={600} style={{ width: "30vh" }} alt="Profile"/>
                   <p className="lead pt-4"> Full Stack & Game Developer with a strong background in AI and Data Science. Experienced in building scalable web systems, multiplayer games, and intelligent agents. Passionate about combining creativity and technology to deliver impactful, high-performance solutions. </p>
                   <a href="#about" className="btn btn-outline-light rounded-pill px-4 py-2">
                     <i className="bi bi-caret-down-fill"></i> More About Me
